b62fbbfa547e2096c24081c371651bcd9b9883ad
cakim <cakim@hansol.com>
2020-11-13 17:36:12 +0900

    보고서 LIST 조회 API 추가

--- externalApiReports/routes/api/infos/index.js
+++ externalApiReports/routes/api/infos/index.js
@@ -1,19 +1,20 @@
 
 const router = require("express").Router();
 
-const agentIdController = require("./agent-id.controller");
-const groupCodeController = require("./group-code.controller");
-const reportsListController = require("./reports-list.controller");
+// const agentIdController = require("./agent-id.controller");
+// const groupCodeController = require("./group-code.controller");
+// const reportsListController = require("./reports-list.controller");
 
-router.get('/agents/:site_cd', agentIdController.read);
-router.get('/groups/:site_cd', groupCodeController.read);
-router.get('/reports/:site_cd', reportsListController.read);
+// router.get('/agents/:site_cd', agentIdController.read);
+// router.get('/groups/:site_cd', groupCodeController.read);
+// router.get('/reports/:site_cd', reportsListController.read);
 
 
-
+const reportsInfos = require('./reports-list.controller');
 const agentInfos = require('./agent-id.controller');
 const groupInfos = require('./group-code.controller');
 const channelInfos = require('./channel-type.controller');
+router.get('/:site_cd/reports', reportsInfos.read);
 router.get('/:site_cd/agents', agentInfos.read);
 router.get('/:site_cd/groups', groupInfos.read);
 router.get('/:site_cd/channels', channelInfos.read);
